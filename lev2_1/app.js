606f953276f06689469b90a947ef17e142655d28
Marius <Marius.Supercode@gmail.com>
2022-12-21 12:53:43 +0100

    2.

--- lev2_1/app.js
+++ lev2_1/app.js
@@ -5,8 +5,7 @@ import jsonData from './data.json' assert { type: "json" };
 const writeContent = () => {
     return new Promise((resolve, reject) => {
         jsonData.forEach((data) => {
-            fs.appendFileSync("data.txt", ` \n ${data.id} - ${data.title}`, (err) => { if (err) reject(err); resolve(); });
-            fs.appendFileSync("data.txt", ` \n ${data.description}`, (err) => { if (err) reject(err); resolve(); });
+            fs.appendFileSync("data.txt", ` \n ${data.id} - ${data.title} \n ${data.description}`, (err) => { if (err) reject(err); resolve(); });
         });
     });
 };
